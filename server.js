db49c0fb29963b3b0d7132392d369ff256b07983
Manuel <NietoSoftwareDeveloper@outlook.com>
2025-11-29 12:12:08 -0500

    Update server.js

--- server.js
+++ server.js
@@ -8,10 +8,6 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 
-
-
-
-//configure env
 dotenv.config();
 
 //databse config
